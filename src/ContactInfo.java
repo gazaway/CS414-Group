aa5a7fb67552b7967d16dab66b81f37b67bbe086
Bryon Berryhill <berryhillb@yahoo.com>
2015-09-30 14:16:39 -0600

    added a contact info class to be used with both customers and employees

---
+++ src/ContactInfo.java
@@ -0,0 +1,16 @@
+
+public class ContactInfo {
+	String name;
+	String address;
+	String phone;
+	
+	public ContactInfo(String name, String address, String phone){
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+	}
+	
+	public String getName(){ return name; }
+	public String getAddress(){ return address;}
+	public String getPhone(){ return phone; }
+}
