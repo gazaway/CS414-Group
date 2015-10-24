a61a5afd1055924450a42193146b25dcfe5751c4
Jim <gazawayj@rams.colostate.edu>
2015-10-24 13:39:34 -0600

    Updated classpath for JUNIT4, added first test file to new junit package

---
+++ src/junit/ManagerInterfaceTest.java
@@ -0,0 +1,84 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ManagerInterfaceTest {
+
+	@Test
+	public void testCreateNewMenu() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testClearWholeMenu() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateSpecialWithItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateSpecialWithPizza() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddItemToSpecial() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveItemFromSpecial() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddPizzaToSpecial() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemovePizzaFromSpecial() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddItemToMenu() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemoveItemFromMenu() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddPizzaSizeToMenu() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemovePizzaSizeFromMenu() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddPizzaToppingToMenu() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemovePizzaToppingFromMenu() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
