70379fa7b4b526ef73ed21e87fe5b81fee36b436
Bryon Berryhill <berryhillb@yahoo.com>
2015-10-27 16:55:58 -0600

    no changes made

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -60,11 +60,8 @@ public class CustomerController {
         //fill menu here
         MenuItem testItem = new MenuItem(1.00, "Test Name", "Test Descr");
         menuItems.add(testItem);
-        
-        PizzaSystem pizzaSystem = new PizzaSystem();
-        // need a menu with items to populate item list
-         * 
-         */
+        *
+        **/
     }
 
     public ObservableList<MenuItem> getOrderItems() {
