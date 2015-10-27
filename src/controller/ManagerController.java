9aa27671bff558c5a5907f6e2db8b35a459189f3
Francisco Aguilera <Falven2000@Hotmail.com>
2015-10-26 20:17:53 -0600

    Added Controller View Hooks

--- src/controller/ManagerController.java
+++ src/controller/ManagerController.java
@@ -2,8 +2,23 @@ package controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import model.MenuItem;
+
+import java.awt.*;
 
 public class ManagerController {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField priceField;
+
+    @FXML
+    private TextField descriptionField;
 
     private static ManagerController instance;
 
@@ -15,5 +30,20 @@ public class ManagerController {
             instance = new ManagerController();
         }
         return instance;
+    }
+
+    @FXML
+    protected void handleAdd(ActionEvent event) {
+    }
+
+    @FXML
+    protected void handleSave(ActionEvent event) {
+    }
+
+    @FXML
+    protected void handleCancel(ActionEvent event) {
+        nameField.setText("");
+        priceField.setText("");
+        descriptionField.setText("");
     }
 }
